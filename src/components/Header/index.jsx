6b3257ccd2798fa14b18f9951b8688de641225ef
л→в <belobrov.lev@gmail.com>
2024-03-12 05:41:15 +0100

    add debug info to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,16 +4,17 @@ import Button from '../../ui/Button';
 
 
 function Header() {
-    const { user, tg } = useTelegram();
+    const { user, tg, platform } = useTelegram();
 
     const onPlatform = () => {
-        tg.showAlert('Текущая платформа: ' + tg.platform());
+        tg.showAlert('Текущая платформа: ' + platform);
     }
 
     return (
         <div className='header tg-theme-header-bg-color'>
             <Button onClick={() => onPlatform}>Узнать платформу</Button>
             <span className='username'>{user?.username}</span>
+            <span className='username'>{platform}</span>
         </div>
     );
 }
